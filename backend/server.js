7b5f841bea68c9047babaceda712a2998baf1707
Bhumika Sharma <bhumikamca25@cs.du.ac.in>
2026-07-06 09:26:26 +0530

    fix cors and vercel config

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,9 @@ const PORT=process.env.PORT||5000;
 dotenv.config();
 
 const app=express();
-app.use(cors());
+app.use(cors({
+  origin: "https://yourpins.vercel.app"
+}));
 app.use(express.json());
 dbConnect();
 app.use("/api/sections", sectionRoutes);
